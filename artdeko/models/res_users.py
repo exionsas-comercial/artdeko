1494c42b8a9d0797e49aabe70106552ef8f161ef
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2019-03-15 00:58:53 -0500

    9

--- artdeko/models/res_users.py
+++ artdeko/models/res_users.py
@@ -3,14 +3,3 @@
 
 from odoo import api, fields, models, SUPERUSER_ID, _
 
-class Users(models.Model):
-    _inherit = 'res.users'
-    #Extend name_get to have the "initials" field in the name
-    
-    #Field to establish the initials of the user
-    initials = fields.Char(string="Iniciales")
-    #Flag to indicate if user is specifier
-    specifier_ok = fields.Boolean(
-        'Es especificador', default=False,
-        help="Indica si el usuario es especificador.")
-    
