1fa0037e89b4d6d465d4c6ea12cfcd75e3199d9f
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2019-03-15 01:01:45 -0500

    10

--- artdeko/models/res_users.py
+++ artdeko/models/res_users.py
@@ -3,3 +3,23 @@
 
 from odoo import api, fields, models, SUPERUSER_ID, _
 
+class Users(models.Model):
+    _inherit = 'res.users'
+    #Extend name_get to have the "initials" field in the name
+    @api.multi
+    def name_get(self):
+        result = []
+        res = super(res.users, self).name_get()        
+        for partner in res:
+            initials = ''
+            for user in self:
+                if user.id == partner.id:
+                    initials = user.initials
+            if self._context.get('show_initials') and initials != '':
+                name = "%s <%s>" % (partner.name, initials)            
+            result.append((partner.id, name))
+        return result
+    #Field to establish the initials of the user
+    initials = fields.Char(string="Iniciales")
+    
+    
