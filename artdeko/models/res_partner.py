b4c42af9ac46b18f84127607acdf89c5c4a77da6
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2019-03-16 10:59:07 -0500

    16

--- artdeko/models/res_partner.py
+++ artdeko/models/res_partner.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from datetime import datetime
 from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_is_zero, float_compare
+from odoo.tools.misc import formatLang
+from odoo.addons import decimal_precision as dp
 
 class Partner(models.Model):
     _inherit = 'res.partner'
     
     #Field to establish the initials of the user
-    initials = fields.Char(string="Iniciales")
+    initials1 = fields.Char(string="Iniciales")
