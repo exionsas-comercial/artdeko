1fea70f27defa30ba0fb9f21d0ed025209a8bdee
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2019-03-16 10:45:13 -0500

    12

--- artdeko/models/res_partner.py
+++ artdeko/models/res_partner.py
@@ -22,7 +22,5 @@ class Partner(models.Model):
     #Field to establish the initials of the user
     initials = fields.Char(string="Iniciales")
     #Flag to indicate if user is specifier
-    specifier_ok = fields.Boolean(
-        'Es especificador', default=False,
-        help="Indica si el usuario es especificador.")
+    specifier_ok = fields.Boolean('Es especificador', default=False,help="Indica si el usuario es especificador.")
     
