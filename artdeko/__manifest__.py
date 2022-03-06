54aa31d81df5d397601e554f9324001b3ffe3631
felipeqvd <felipeqvd@hotmail.com>
2022-03-06 13:24:22 -0500

    Creating module

---
+++ artdeko/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'ARTDEKO',
+    'depends': [
+        'base_setup'
+    ],
+    'css': ['static/src/css/crm.css'],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
