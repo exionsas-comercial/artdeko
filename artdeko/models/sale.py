9c097e6f419e7d577118713717dc3c697e21aa94
felipeqvd <felipeqvd@hotmail.com>
2022-03-15 13:46:49 -0500

    fix access

--- artdeko/models/sale.py
+++ artdeko/models/sale.py
@@ -7,13 +7,13 @@ class SaleOrder(models.Model):
     #Campo para tener el conteo de las ordenes de compra que se han generado por la venta
     purchase_count = fields.Integer(string='Ordenes de compra', compute='_compute_purchase_ids')
     #Campo para tener en un string los códigos de las ordenes de compra que se han generado por la venta
-    purchase_string = fields.Char(string='Ordenes de compra', compute='_compute_purchase_ids')
+    purchase_string = fields.Char(string='Códigos ordenes de compra', compute='_compute_purchase_ids')
     #Campo para tener el conteo de las recepciones relacionadas con la venta através de las compras
     receipt_count = fields.Integer(string='Recepciones', compute='_compute_receipt_ids')
     #Campo para asociar las divisiones
     division = fields.Many2one('artdeko.division', string="División", null=True)
     #Campo para asociar los canales
-    team_id = fields.Many2one('crm.team', string='Canal', oldname='section_id', null=True)
+    team_id = fields.Many2one('crm.team', string='Canal')
     #Campo para asociar los especificadores
     specifier_id = fields.Many2one('res.users', string='Especificador', index=True, null=True, domain=[('specifier_ok', '=', True)], context={'show_initials': True})
     
@@ -22,10 +22,10 @@ class SaleOrder(models.Model):
         Calculate the quantity of the purchases for each sale order and update the data base.
         '''
         purchases_names = ""
-        purchases = self.env['purchase.order'].search([('sale_order', '=', self.id)])
+        purchases = self.env['purchase.order'].sudo().search([('sale_order', '=', self.id)])
         self.purchase_count = len(purchases)
         if self.purchase_count == 1:
-            purchases_names = self.env['purchase.order'].search([('sale_order', '=', self.id)], limit=1).name
+            purchases_names = self.env['purchase.order'].sudo().search([('sale_order', '=', self.id)], limit=1).name
         elif self.purchase_count > 1:                
             for purchase in purchases:
                 purchases_names = purchases_names + purchase.name + ", "
@@ -35,5 +35,5 @@ class SaleOrder(models.Model):
         '''
         Calculate the quantity of the pickings asociate with the purchases for each sale order and update the data base.
         '''
-        receipts = self.env['purchase.order'].search_read([('sale_order', '=', self.id)], ['picking_count'])
+        receipts = self.env['purchase.order'].sudo().search_read([('sale_order', '=', self.id)], ['picking_count'])
         self.receipt_count = sum([item['picking_count'] for item in receipts])
