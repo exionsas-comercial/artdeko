c5208ac6b448c5c12285099da0b121447c8eef00
felipeqvd <felipeqvd@hotmail.com>
2022-03-18 00:49:41 -0500

    back to test2

--- artdeko/models/__init__.py
+++ artdeko/models/__init__.py
@@ -3,4 +3,3 @@ from . import res_users
 from . import artdeko_models
 from . import purchase
 from . import sale
-from . import stock_picking
--- artdeko/models/res_partner.py
+++ artdeko/models/res_partner.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, models
 
 class Partner(models.Model):
     _inherit = 'res.partner'
@@ -10,10 +10,10 @@ class Partner(models.Model):
         for partner in res:
             partner_id, name = partner
             initials = ''
-            users = self.env['res.users'].search([('partner_id', '=', partner_id)])
+            users = self.env['res.users'].sudo().search([('partner_id', '=', partner_id)])
             for user in users:                
                 initials = user.initials
-            if self._context.get('show_initials') and initials != '':
+            if self._context.get('show_initials') and initials:
                 name = "%s <%s>" % (name, initials)            
             result.append((partner_id, name))
         return result
