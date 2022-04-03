0b392b972250df2e071625fa17e9848889b838e9
felipeqvd <felipeqvd@hotmail.com>
2022-04-03 18:53:54 -0500

    sale quotation

--- artdeko/__manifest__.py
+++ artdeko/__manifest__.py
@@ -17,7 +17,10 @@
     'license': 'Other proprietary',
     'data': [
         'security/ir.model.access.csv',
-        'views/artdeko_views.xml'
-
+        'views/artdeko_views.xml',
+        'report/artdeko_paper_formats.xml',
+        'report/artdeko_layouts.xml',
+        'report/artdeko_sale_templates.xml',
+        'report/artdeko_reports.xml'
     ]
 }
