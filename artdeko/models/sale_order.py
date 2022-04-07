1def0e2511d7e0067012b15b111d8c0c7ab8abd8
felipeqvd <felipeqvd@hotmail.com>
2022-04-07 15:14:54 -0500

    adding specifier

--- artdeko/models/sale_order.py
+++ artdeko/models/sale_order.py
@@ -8,3 +8,5 @@ class SaleOrder(models.Model):
     division = fields.Many2one('artdeko.division', string="División", null=True)
     #Campo para asociar los canales
     team_id = fields.Many2one('crm.team', string='Canal', null=True)
+    #Campo para asociar los especificadores
+    specifier_id = fields.Many2one('res.users', string='Especificador', index=True, null=True, domain=[('specifier_ok', '=', True)], context={'show_initials': True})
