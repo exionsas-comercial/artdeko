13e76eef71de73a77cae457c7b5fc614400fd075
felipeqvd <felipeqvd@hotmail.com>
2022-04-08 02:31:55 -0500

    merge

--- artdeko/__manifest__.py
+++ artdeko/__manifest__.py
@@ -18,6 +18,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/artdeko_views.xml',
+        'views/users_views.xml',
         'views/stock_picking_views.xml',
         'report/artdeko_paper_formats.xml',
         'report/artdeko_layouts.xml',
