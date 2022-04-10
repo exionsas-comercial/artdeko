85b389be04429d9b3985302cc518a608406b68b4
felipeqvd <felipeqvd@hotmail.com>
2022-04-10 17:09:12 -0500

    fix purchase report

--- artdeko/__manifest__.py
+++ artdeko/__manifest__.py
@@ -23,6 +23,7 @@
         'report/artdeko_paper_formats.xml',
         'report/artdeko_layouts.xml',
         'report/artdeko_sale_templates.xml',
+        'report/artdeko_purchase_order_templates.xml',
         'report/artdeko_reports.xml'
     ]
 }
