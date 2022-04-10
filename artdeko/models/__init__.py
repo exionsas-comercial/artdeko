cbf952c326a0eb09d445e9493181ad86bc207748
felipeqvd <felipeqvd@hotmail.com>
2022-04-09 20:33:48 -0500

    fix merge

--- artdeko/models/__init__.py
+++ artdeko/models/__init__.py
@@ -2,8 +2,5 @@ from . import res_users
 from . import artdeko_models
 from . import purchase
 from . import sale_order
-<<<<<<< HEAD
 from . import crm_lead
-=======
 from . import stock_picking
->>>>>>> clean-c1
